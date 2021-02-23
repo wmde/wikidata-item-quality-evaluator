f8b1140fd63f3993a7bb7fe3159a75de54a8edc5
Adam Hammad <mail@adamham.dev>
2021-02-23 16:49:47 +0100

    feat: init vuex store (T275302)

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,30 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    results: []
+  },
+  mutations: {
+    updateResults(state, payload) {
+      state.results = payload;
+    }
+  },
+  getters: {
+    totalAverageScore(state) {
+      const total = state.results
+        .filter((res: any) => !res.missing)
+        .reduce((acc, result: any) => (acc += result.score), 0);
+      const average =
+        total / state.results.filter((res: any) => !res.missing).length;
+      return average || 0;
+    },
+    missingResults(state) {
+      return state.results.filter((res: any) => res.missing);
+    },
+    validResults(state) {
+      return state.results.filter((res: any) => !res.missing);
+    }
+  },
   actions: {},
   modules: {}
 });
