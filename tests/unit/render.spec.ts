3635d2e2c5e2cfa0803e068c5a30201935b7983c
Adam Hammad <mail@adamham.dev>
2021-02-25 14:14:35 +0100

    feat: add init test and deploy script (T275382)

---
+++ tests/unit/render.spec.ts
@@ -0,0 +1,19 @@
+import { createLocalVue, mount } from "@vue/test-utils";
+import App from "@/App.vue";
+import { BContainer } from "bootstrap-vue";
+const localVue = createLocalVue();
+
+localVue.component("b-container", BContainer);
+
+describe("App.vue", () => {
+  it("renders the application", () => {
+    const title = "Itemset Quality Tool";
+    const wrapper = mount(App, {
+      stubs: ["router-view"],
+      components: {
+        BContainer
+      }
+    });
+    expect(wrapper.text()).toMatch(title);
+  });
+});
