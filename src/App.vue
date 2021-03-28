9ac966e629935b8ea290beec6b46bbd75bd92c03
Adam Hammad <mail@adamham.dev>
2021-03-28 15:44:00 +0200

    chore: fix header/footer

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div id="app">
     <b-container class="mt-4 mb-4">
-      <img
-        class="logo mb-2"
-        src="https://upload.wikimedia.org/wikipedia/commons/6/66/Wikidata-logo-en.svg"
-      />
-      <h1>Item Quality Evaluator Tool</h1>
+      <b-row align-v="center" class="mb-4 mx-0">
+        <img
+          class="logo mr-2"
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Wikidata-logo_S.svg"
+        />
+        <h1 class="mb-0">Item Quality Evaluator Tool</h1>
+      </b-row>
       <router-view />
+      <footer class="mt-5">
+        <span>Made by the Wikimedia Deutschland Wikidata team.</span>
+        <p>
+          Licensed under
+          <a
+            href="https://github.com/wmde/wikidata-item-quality-evaluator/blob/main/LICENSE"
+            target="_blank"
+            >BSD 3-Clause License</a
+          >.
+          <a
+            href="https://github.com/wmde/wikidata-item-quality-evaluator"
+            target="_blank"
+            >View Source</a
+          >.
+          <a
+            href="https://phabricator.wikimedia.org/tag/item_quality_evaluator/"
+            target="_blank"
+            >Report an issue</a
+          >.
+        </p>
+      </footer>
     </b-container>
   </div>
 </template>
 <style lang="scss" scoped>
 img.logo {
-  width: 150px;
-  float: right;
+  height: 2rem;
 }
 </style>
 <script lang="ts">
